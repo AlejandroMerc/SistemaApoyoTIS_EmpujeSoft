7d5555ae27485bb4d095f6e1bcb29d260bd1d465
AlejandroMerc <66449813+AlejandroMerc@users.noreply.github.com>
2021-10-22 22:05:51 -0400

    agregar dinamicamente miembros a la registroGE, sin BD

--- resources/views/registerGE.blade.php
+++ resources/views/registerGE.blade.php
@@ -11,7 +11,7 @@
                         @csrf
                         <div class="form-group row ">
                         <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre Corto') }}</label>
-                            <div class="col-md-6">                                 
+                            <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
@@ -23,7 +23,7 @@
 
                         <div class="form-group row">
                         <label for="nameLarge" class="col-md-4 col-form-label text-md-right">{{ __('Nombre Largo') }}</label>
-                            <div class="col-md-6">                               
+                            <div class="col-md-6">
                                     <input id="nameLarge" type="text" class="form-control @error('name') is-invalid @enderror" name="nameLarge" value="{{ old('nameLarge') }}" required autocomplete="nameLarge" autofocus>
                                         @error('lastname')
                                             <span class="invalid-feedback" role="alert">
@@ -33,9 +33,9 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">    
+                        <div class="form-group row">
                         <label for="telf" class="col-md-4 col-form-label text-md-right">{{ __('Telefono') }}</label>
-                            <div class="col-md-6">                               
+                            <div class="col-md-6">
                                     <input id="telf" type="number" class="form-control @error('email') is-invalid @enderror" min="190000000" max="999999999" name = "telf" value="{{ old('telf') }}" required autocomplete="telf">
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -46,11 +46,11 @@
                         </div>
 
                         <div class="form-group row">
-                        <label for="sociedad" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de Sociedad') }}</label> 
-                            <div class="col-md-6 ">                                       
+                        <label for="sociedad" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de Sociedad') }}</label>
+                            <div class="col-md-6 ">
                                 <select id="sociedad"  class="form-control @error('email') is-invalid @enderror" name="sociedad" value="{{ old('sociedad') }}" required autocomplete="sociedad">
                                     <option value="volvo">S.A</option>
-                                    <option value="volvo">ASL</option>
+                                    <option value="volvo2">ASL</option>
                                 </select>
                                 @error('required')
                                     <span class="invalid-feedback" role="alert">
@@ -60,8 +60,8 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label> 
-                            <div class="col-md-6">                   
+                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
+                            <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                                         @error('email')
                                          <span class="invalid-feedback" role="alert">
@@ -73,7 +73,7 @@
 
                         <div class="form-group row">
                             <label for="direc" class="col-md-4 col-form-label text-md-right">{{ __('Direccion') }}</label>
-                            <div class="col-md-6">            
+                            <div class="col-md-6">
                                     <input id="direc" type="text" class="form-control @error('name') is-invalid @enderror" name="direc" value="{{ old('direc') }}" required autocomplete="direc" autofocus>
                                         @error('name')
                                             <span class="invalid-feedback" role="alert">
@@ -82,40 +82,48 @@
                                         @enderror
                             </div>
                         </div>
-                        
-                        <div class="form-group row">   
+
+                        <div class="form-group row">
                             <label for="aniadirMiembros" class="col-md-4 col-form-label text-md-right">{{ __('Añadir Miembros') }}</label>
-                            <div class="col-md-6">                    
+                            <div class="col-md-6">
                                     <div class="row" >
                                         <div class="col-auto">
-                                            <input id="aniadirMiembros" type="email" class="form-control @error('email') is-invalid @enderror" name="aniadirMiembros" value="{{ old('aniadirMiembros') }}" required autocomplete="aniadirMiembros">
+                                            <input id="aniadirMiembros" type="email" class="form-control @error('email') is-invalid @enderror" name="aniadirMiembros" value="{{ old('aniadirMiembros') }}" autocomplete="aniadirMiembros">
                                                 @error('email')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
                                                 @enderror
-                                        </div> 
-                                        <div class="col-auto"> 
-                                            <button type="submit" class="btn btn-primary ">
-                                                    {{ __('Añadir') }}
-                                            </button> 
                                         </div>
-                                   </div>           
+                                        <div class="col-auto">
+                                            <button type="button" class="btn btn-primary " onclick="addMember()">
+                                                    {{ __('Añadir') }}
+                                            </button>
+                                        </div>
+                                   </div>
                             </div>
                         </div>
 
-                       
-                        <div class="form-group row">
-                            <label for="table" class="col-md-4 col-form-label text-md-right">{{ __('Tabla') }}</label>
-                            <div class="col-md-6">                               
+
+                        <div id="listMember">
+
+                            {{-- <label for="table" class="col-md-4 col-form-label text-md-right">{{ __('Tabla') }}</label>
+                            <div class="col-md-6">
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
                                             <th>Nombre</th>
                                             <th>Correo</th>
                                             <th>Rol</th>
-                                        </tr>    
+                                        </tr>
                                     </thead>
+                                    @foreach ($members as $member)
+                                        <tr>
+                                            <th>{{ $member['name'] }}</th>
+                                            <th>{{ $member['email'] }}</th>
+                                            <th>{{ $member['rol'] }}</th>
+                                        </tr>
+                                    @endforeach
                                     <tbody>
                                         <tr>
                                             <th>1a</th>
@@ -129,9 +137,9 @@
                                         </tr>
                                     </tbody>
                                 </table>
-                            </div>
+                            </div> --}}
                         </div>
-  
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -145,4 +153,48 @@
         </div>
     </div>
 </div>
+
+<script>
+
+    var id = 1;
+
+    function addMember() {
+        var memberEmail = document.getElementById('aniadirMiembros').value;
+
+        // en esta parte debemos verificar mediante la base de datos, que el correo cumple con las especificaciones de usuario
+        // no se como verificar que el correo esta cumpliendo los formatos requeridos
+        // no se como mostrar alerta de errores :'v
+
+        var divRowTag  =document.createElement('div');
+        divRowTag.setAttribute('class', 'form-group row');
+
+        var element = document.getElementById('listMember');
+
+        var labelTag = document.createElement('label');
+        labelTag.setAttribute('for', 'member'+id);
+        labelTag.setAttribute('class', 'col-md-4 col-form-label text-md-right');
+        labelTag.innerHTML = "Miembro " + id;
+
+        var divTag = document.createElement('div');
+        divTag.setAttribute('class', 'col-md-6');
+
+        var inputTag = document.createElement('input');
+        inputTag.setAttribute('id', 'member'+id);
+        inputTag.setAttribute('type', 'text');
+        inputTag.setAttribute('class', 'form-control');
+        inputTag.setAttribute('name', 'member'+id);
+        inputTag.setAttribute('value', memberEmail);
+        inputTag.setAttribute('autocomplete', 'member'+id);
+        inputTag.toggleAttribute('disabled');
+        inputTag.appendChild(document.createTextNode(memberEmail));
+
+        console.log("id: "+inputTag.getAttribute('id'));
+
+        id++;
+        divRowTag.appendChild(labelTag)
+        divTag.appendChild(inputTag);
+        divRowTag.appendChild(divTag);
+        element.appendChild(divRowTag);
+    }
+</script>
 @endsection
