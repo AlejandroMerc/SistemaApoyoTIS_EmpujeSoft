0765700c3e9e7e3878c3c57fa82f5a28baaa3c29
FCamacho96 <66483613+FCamacho96@users.noreply.github.com>
2021-11-01 19:28:05 -0400

    Correccion de listado de grupoempresas

--- app/Http/Controllers/ListGEController.php
+++ app/Http/Controllers/ListGEController.php
@@ -14,13 +14,11 @@ class ListGEController extends Controller
     //
     public function showListGE()
     {
-        $grupoEmpresas= Grupoempresa::join('users','users.id','grupoempresas.rep_legal_id')
-        ->join('estudiantes','estudiantes.user_id','users.id')
+        $grupoEmpresas= Grupoempresa::join('estudiantes','estudiantes.id','grupoempresas.rep_legal_id')
         ->join('grupos','grupos.id','estudiantes.grupo_id')
         ->join('semestres','semestres.id','grupos.semestre_id')
         ->select('grupoempresas.id','grupoempresas.nombre_corto','grupoempresas.nombre_largo','semestres.year','semestres.periodo')
         ->get();
-        
         
         return view('listGE', compact('grupoEmpresas'));
     }
