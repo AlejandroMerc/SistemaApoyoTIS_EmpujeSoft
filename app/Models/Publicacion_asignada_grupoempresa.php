6a7660edd338020ac5f320673f35c6d656e583ee
FCamacho96 <66483613+FCamacho96@users.noreply.github.com>
2021-11-18 21:10:40 -0400

    migraciones y modelos para espacios de trabajo

---
+++ app/Models/Publicacion_asignada_grupoempresa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publicacion_asignada_grupoempresa extends Model
+{
+    use HasFactory;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var string[]
+    */
+    protected $fillable = [
+       'publicacion_id',
+       'grupoempresa_id'
+    ];
+}
