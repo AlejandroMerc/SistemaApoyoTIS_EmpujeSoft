b1ab1587882d646d2098f878282fdf764e535293
FCamacho96 <66483613+FCamacho96@users.noreply.github.com>
2021-11-26 23:19:27 -0400

    Migracion y modelo para plantillas

---
+++ database/migrations/2021_11_27_030958_create_plantillas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlantillas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plantillas', function (Blueprint $table) {
+            $table->id();
+            $table->string('html_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plantillas');
+    }
+}
