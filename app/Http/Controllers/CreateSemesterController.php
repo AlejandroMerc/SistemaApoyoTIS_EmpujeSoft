08a4494e11dd46b450b0100a78645fe4783a35f9
rafa20506 <nogalesrafael1@gmail.com>
2021-12-01 14:26:47 -0400

    guardar en base de datos semestre

--- app/Http/Controllers/CreateSemesterController.php
+++ app/Http/Controllers/CreateSemesterController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Redirect;
+use App\Models\Semestre;
 use Illuminate\Http\Request;
 
 class CreateSemesterController extends Controller
 {
     public function createSemester(){
         return view('createSemester');
+    }
+    public function store(Request $request){
+        $request->validate([
+            'anio' => ['required', 'numeric'],
+            'periodo'=>['required','numeric'],
+            'deathline'=>['required','date'],
+            'deathline2'=>['required','date']
+        ]);
+        $semest=new Semestre;
+        $semest->year=$request->anio;
+        $semest->periodo=$request->periodo;
+        $semest->fecha_inicio=$request->deathline;
+        $semest->fecha_fin=$request->deathline2;
+        if($semest->save()){
+            return Redirect::back()->with('message','Operation Successful !');
+        }else{
+            return "no se guardo";
+        }
+
     }
 }
