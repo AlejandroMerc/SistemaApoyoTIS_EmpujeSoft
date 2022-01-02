6d947e8f01fee9af76c3ca27aa948c5c3f602583
BillieVC <manuel.vargas2013.jmvc@gmail.com>
2022-01-01 22:05:26 -0400

    correccion archivos entregas

--- app/Http/Controllers/TurnInActivityController.php
+++ app/Http/Controllers/TurnInActivityController.php
@@ -34,7 +34,8 @@ class TurnInActivityController extends Controller
         $hayEntregado=Entrega::where('actividad_id','=',$actividad->id)->where('grupoempresa_id','=',$estudiante->grupoempresa_id)->first();
         if($hayEntregado!=null){
             $entregado=true;
-            $adjuntosEntrega=Adjunto_entrega::where('entrega_id','=',$hayEntregado->id)
+            $adjuntosEntrega=Entrega::where('actividad_id','=',$actividad->id)
+                                ->join('adjunto_entregas','entrega_id','=','entregas.id')
                                 ->join('adjuntos','adjuntos.id','=','adjunto_entregas.adjunto_id')->get();
             $hayEntregado['adjuntosEntrega']=$adjuntosEntrega;
         }
