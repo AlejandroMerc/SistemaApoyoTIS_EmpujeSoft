cc35e64aab4ab7c15aa476d1a829a1c90f319772
AlejandroMerc <66449813+AlejandroMerc@users.noreply.github.com>
2022-01-04 10:28:21 -0400

    correccion home vacio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,50 +1,51 @@
 @extends('layouts.home_layout')
 
 @section('content')
-    
+
 
 
 <div class="containter-fluid">
     <br>
     <div class="col-md-12">
-      @foreach ($publications as $publication)
-      <div class="card border border-secondary border-1 rounded-lg"  value={{$publication->id}}>
-        @if ($publication->tipo=="Publicación")
-            <div class="card-header border border-1 bg-opacity-10 bg-secondary text-white" style="opacity: .8;">
-              <i class="fas fa-bullhorn text-white-30"></i>
-              {{$publication->titulo_publicacion}}
-            </div>
+        @if ( $publications->isEmpty() )
+         {{-- aqui poner bienvenida --}}
         @else
-        <div class="card-header border border-1 text-white bg-info">
-          <i class="fas fa-tasks text-white-30"></i>
-          {{$publication->titulo_publicacion}}
-        </div>
-        @endif
+            @foreach ($publications as $publication)
+                <div class="card border border-secondary border-1 rounded-lg"  value={{$publication->id}}>
+                    @if ($publication->tipo=="Publicación")
+                        <div class="card-header border border-1 bg-opacity-10 bg-secondary text-white" style="opacity: .8;">
+                            <i class="fas fa-bullhorn text-white-30"></i>
+                            {{$publication->titulo_publicacion}}
+                        </div>
+                    @else
+                        <div class="card-header border border-1 text-white bg-info">
+                            <i class="fas fa-tasks text-white-30"></i>
+                            {{$publication->titulo_publicacion}}
+                        </div>
+                    @endif
 
-        <div class="card-body  border border-1">
+                <div class="card-body  border border-1">
 
-          <p class="card-text">{{$publication->descripcion_publicacion}}</p>
-          @foreach($publication->adjuntos as $adjunto)
-            <p><a href="{{asset($adjunto->path)}}" class="card-link"><i class="fas fa-paperclip"></i> {{$adjunto->name}}</a></p>
-          @endforeach
-          @if ($publication->tipo=="Actividad")
-          @if ($user_type == 'asesor_tis')
-          <a href="{{route('verRespuestasDos',['publicacion_id' => $publication->id])}}" class="btn btn-primary">Ver Respuestas</a>
-          @else
-          <a href="{{route('responderActividad',['publicacion_id'=>$publication->id])}}" class="btn btn-primary">Responder</a>
-          @endif
-          <p class="card-text">Fecha de Entrega: {{$publication->fechaDeEntrega}}</p>
-          @endif
+                <p class="card-text">{{$publication->descripcion_publicacion}}</p>
+                @foreach($publication->adjuntos as $adjunto)
+                    <p><a href="{{asset($adjunto->path)}}" class="card-link"><i class="fas fa-paperclip"></i> {{$adjunto->name}}</a></p>
+                @endforeach
+                @if ($publication->tipo=="Actividad")
+                    @if ($user_type == 'asesor_tis')
+                        <a href="{{route('verRespuestasDos',['publicacion_id' => $publication->id])}}" class="btn btn-primary">Ver Respuestas</a>
+                    @else
+                        <a href="{{route('responderActividad',['publicacion_id'=>$publication->id])}}" class="btn btn-primary">Responder</a>
+                    @endif
+                    <p class="card-text">Fecha de Entrega: {{$publication->fechaDeEntrega}}</p>
+                @endif
+                </div>
+                <div class="card-footer text-muted border border-1 ">
+                    {{$publication->name}} {{$publication->lastname}} a las: {{$publication->fecha_publicacion}}
+                </div>
+            </div>
 
-
-        </div>
-        <div class="card-footer text-muted border border-1 ">
-
-         {{$publication->name}} {{$publication->lastname}} a las: {{$publication->fecha_publicacion}}
-        </div>
-      </div>
-
-  @endforeach
+        @endforeach
+    @endif
     </div>
 
 </div>
