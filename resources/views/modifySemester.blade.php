605e72ecf3d111416352ed069a0e3da21e6facbf
ipoandres08 <andresiporre07@gmail.com>
2022-01-07 19:58:30 -0400

    correciones titulos

--- resources/views/modifySemester.blade.php
+++ resources/views/modifySemester.blade.php
@@ -14,21 +14,21 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header d-flex justify-content-between">
-                    <span class="h1">@if ($started) Modificar Semestre @else Semestre pendiente @endif {{ $semester->year }}/{{ $semester->periodo }}</span>
+                <div class="card-header bg-primary text-white d-flex justify-content-between">
+                    <span style="color:white"class="h4">@if ($started) Modificar Semestre @else Semestre pendiente @endif {{ $semester->year }}/{{ $semester->periodo }}</span>
                     <a class="rounded-pill bg-secondary p-2" type="button" onclick="showAlert(@if($started)true @else false @endif)">
-                        <span class="h3">
+                        <span class="h6">
                             <i class="fas fa-question text-white"></i>
                         </span>
                     </a>
                 </div>
-                   <div class="card-body">
+                   <div class="card-body"><strong>
                     <form method="POST" action="@if ($started){{route('semestre.modif-start-data')}}@else{{route('semestre.modif-data')}}@endif">
                         @csrf
                         <input hidden type="number" name="semester_id" value={{$semester->id}}>
 
                         <div class="form-group row">
-                         <label for="telefono_ge" class="col-md-4 col-form-label text-md-right">{{ __('Año ') }}</label>
+                         <label for="telefono_ge" class="col-md-4 col-form-label text-md-right">{{ __('*Año ') }}</label>
                             <div class="col-md-6">
                                     <input @if( $started) disabled @endif id="anio" type="number" class="form-control @error('anio') is-invalid @enderror" name = "anio" value="{{ old('anio', $semester->year) }}" required autocomplete="anio">
                                     @error('anio')
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group row">
-                         <label for="periodo" class="col-md-4 col-form-label text-md-right">{{ __('Periodo') }}</label>
+                         <label for="periodo" class="col-md-4 col-form-label text-md-right">{{ __('*Periodo') }}</label>
                              <div class="col-md-6">
                                     <input @if( $started) disabled @endif id="periodo" type="number" class="form-control @error('periodo') is-invalid @enderror" name = "periodo" value="{{ old('periodo', $semester->periodo) }}" required autocomplete="periodo">
                                     @error('periodo')
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="form-group row">
-                          <label for="FechaInicio" class="col-md-4 col-form-label text-md-right">{{ __('Fecha Inicial') }}</label>
+                          <label for="FechaInicio" class="col-md-4 col-form-label text-md-right">{{ __('*Fecha Inicial') }}</label>
                             <div class="col-md-6">
                                 <input @if( $started) disabled @endif type="datetime-local" id="FechaInicio" class="form-control @error('FechaInicio') is-invalid @enderror" name="FechaInicio" min="{{$current_year}}-01-01T00:00" max="{{$current_year+1}}-12-31T23:59" value="{{ old('FechaInicio', $semester->fecha_inicio.'T00:00') }}" required>
                                 @error('FechaInicio')
@@ -64,7 +64,7 @@
                         </div>
 
                         <div class="form-group row">
-                          <label for="FechaFin" class="col-md-4 col-form-label text-md-right">{{ __('Fecha Final') }}</label>
+                          <label for="FechaFin" class="col-md-4 col-form-label text-md-right">{{ __('*Fecha Final') }}</label>
                             <div class="col-md-6">
 
                                 <input type="datetime-local" id="FechaFin" class="form-control @error('FechaFin') is-invalid @enderror" name="FechaFin" min="@if($started){{ __($today.'T00:00')}}@else{{ __(($current_year)."-01-01T00:00")}}@endif" max="@if($started){{$semester->year+1}}@else{{$current_year+1}}@endif-12-31T23:59" value="{{ old('FechaFin',$semester->fecha_fin.'T00:00') }}" required>
